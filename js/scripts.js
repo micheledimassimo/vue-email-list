891f56e77f2b39812c76a9c91b02aefe012699ba
micheledimassimo <mcldms@outlook.it>
2024-09-02 17:59:14 +0200

    v1.0

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,28 @@ const { createApp } = Vue
     createApp({
         data() {
             return {
-                message: 'Template Vue'
+                message: 'Vue Email List',
+                email: ['']
+            }
+        },
+        created() {
+            
+            //ciclo la richiesta API 10 volte per ottenere 10 mail diverse 
+            for (let i = 0; i < 10; i++) {
+                axios 
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((resp) => {
+                    console.log(resp.data);
+                    //pusho gli elementi generati nell'array vuoto
+                    this.email.push(resp.data.response);
+                    
+                });
+               
+                
             }
         }
+        
     }).mount('#app');
+
+
+    
